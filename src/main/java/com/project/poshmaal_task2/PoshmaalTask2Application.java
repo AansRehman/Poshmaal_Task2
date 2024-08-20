a5959468b8f53a551cbbc1e5e3226a8898ffa218
AansRehman <aansrehman10@gmail.com>
2024-08-20 18:31:13 +0500

    first commit

---
+++ src/main/java/com/project/poshmaal_task2/PoshmaalTask2Application.java
@@ -0,0 +1,13 @@
+package com.project.poshmaal_task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshmaalTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoshmaalTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poshmaal_task2/PoshmaalTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poshmaal_task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoshmaalTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
