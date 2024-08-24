44eeae55506578a8094c380ae0d59677cba79c01
AansRehman <aansrehman10@gmail.com>
2024-08-24 21:45:45 +0500

    test cases done with auth

--- src/test/java/com/project/poshmaal_task2/ArtistControllerTest.java
+++ src/test/java/com/project/poshmaal_task2/ArtistControllerTest.java
@@ -1,6 +1,7 @@
 package com.project.poshmaal_task2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.poshmaal_task2.config.SecurityConfig;
 import com.project.poshmaal_task2.controller.ArtistController;
 import com.project.poshmaal_task2.model.Artist;
 import com.project.poshmaal_task2.repository.ArtistRepository;
@@ -12,13 +13,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.Mockito.when;
-//@Import(SecurityConfig.class) // for authentication testcases
+@Import(SecurityConfig.class) // for authentication testcases
 
 @WebMvcTest(ArtistController.class)
 public class ArtistControllerTest {
@@ -38,6 +41,7 @@ public class ArtistControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "staff@gmail.com", password="Staff@10", roles = {"STAFF"})
     public void testFindById_ArtistExists() throws Exception {
         // Arrange
         Long artistId = 1L;
@@ -53,7 +57,7 @@ public class ArtistControllerTest {
         // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.get("/artist/findArtistById/{id}", artistId)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isFound())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(artistId))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("John"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Doe"))
@@ -62,6 +66,7 @@ public class ArtistControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "staff@gmail.com", password="Staff@10", roles = {"STAFF"})
     public void testFindById_ArtistDoesNotExist() throws Exception {
         // Arrange
         Long artistId = 2L;
@@ -71,10 +76,11 @@ public class ArtistControllerTest {
         // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.get("/artist/findArtistById/{id}", artistId)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
     @Test
+    @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
     public void testDeleteArtist_ArtistExists() throws Exception {
         // Arrange
         Long artistId = 1L;
@@ -83,11 +89,12 @@ public class ArtistControllerTest {
         // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
                 .andExpect(MockMvcResultMatchers.content().string("Artist and their artworks deleted successfully"));
     }
 
     @Test
+    @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
     public void testDeleteArtist_ArtistDoesNotExist() throws Exception {
         // Arrange
         Long artistId = 2L;
@@ -96,30 +103,30 @@ public class ArtistControllerTest {
         // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.content().string("Failed to delete artist"));
     }
 
- /**    @Test
-public void testDeleteArtist_ArtistExistsButNotAuthenticated() throws Exception {
-    // Arrange
-    Long artistId = 1L;
+    @Test
+    public void testDeleteArtist_ArtistExistsButNotAuthenticated() throws Exception {
+        // Arrange
+        Long artistId = 1L;
 
-    // Act & Assert
-    mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized()); // Expect Unauthorized status (401)
-}
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized()); // Expect Unauthorized status (401)
+    }
 
-@Test
-@WithMockUser(username = "user", roles = {"USER"})
-public void testDeleteArtist_ArtistExistsButNotAuthorized() throws Exception {
-    // Arrange
-    Long artistId = 1L;
+    @Test
+    @WithMockUser(username = "staff@gmail.com", password="Staff@10", roles = {"STAFF"})
+    public void testDeleteArtist_ArtistExistsButNotAuthorized() throws Exception {
+        // Arrange
+        Long artistId = 1L;
 
-    // Act & Assert
-    mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(MockMvcResultMatchers.status().isForbidden()); // Expect Forbidden status (403)
-} **/
-}
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.delete("/artist/deleteArtist/{id}", artistId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden()); // Expect Forbidden status (403)
+    }
+    }
--- src/test/java/com/project/poshmaal_task2/ArtworkControllerTest.java
+++ src/test/java/com/project/poshmaal_task2/ArtworkControllerTest.java
@@ -1,15 +1,21 @@
 package com.project.poshmaal_task2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.poshmaal_task2.config.SecurityConfig;
 import com.project.poshmaal_task2.controller.ArtworkController;
 import com.project.poshmaal_task2.model.Artwork;
 import com.project.poshmaal_task2.repository.ArtworkRepository;
+import com.project.poshmaal_task2.repository.ArtistRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -17,6 +23,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@Import(SecurityConfig.class) // for authentication testcases
 
 @WebMvcTest(controllers = ArtworkController.class)
 public class ArtworkControllerTest {
@@ -27,25 +35,38 @@ public class ArtworkControllerTest {
     @MockBean
     private ArtworkRepository artworkRepository;
 
+    @MockBean
+    private ArtistRepository artistRepository;
+
     @Autowired
     private ObjectMapper objectMapper;
 
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
+    @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
     public void testAddArtwork_Conflict() throws Exception {
 
         Artwork artwork = new Artwork();
         artwork.setTitle("Artwork Title");
+        artwork.setArtist_id(1L);
+        artwork.setPrice(1.00);
 
         when(artworkRepository.addArt(any(Artwork.class))).thenReturn(0);
 
         mockMvc.perform(post("/artwork/addArtwork")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(artwork)))
-                .andExpect(status().isNotFound()) 
+                .andExpect(status().isBadRequest())
                 .andExpect(content().string("Failed to add artwork"));
     }
 
     @Test
+    @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
     public void testAddArtwork_InvalidData() throws Exception {
 
         Artwork artwork = new Artwork();
@@ -57,10 +78,59 @@ public class ArtworkControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
     public void testAddArtwork_NoData() throws Exception {
         mockMvc.perform(post("/artwork/addArtwork")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
+
+
+     @Test
+     @WithMockUser(username = "buyer@gmail.com", password="Buyer@10", roles = {"BUYER"})
+     public void testAddArtwork_AuthenticatedAndAuthorized() throws Exception {
+
+     Artwork artwork = new Artwork();
+     artwork.setTitle("ArtworkTitle");
+     artwork.setPrice(1.00);
+     artwork.setArtist_id(1L);
+
+     when(artworkRepository.addArt(any(Artwork.class))).thenReturn(1);
+
+     mockMvc.perform(post("/artwork/addArtwork")
+     .contentType(MediaType.APPLICATION_JSON)
+     .content(objectMapper.writeValueAsString(artwork)))
+     .andExpect(status().isCreated())
+     .andExpect(content().string("Artwork added successfully"));
+     }
+
+     @Test
+     public void testAddArtwork_NotAuthenticated() throws Exception {
+
+     Artwork artwork = new Artwork();
+     artwork.setTitle("Artwork Title");
+
+     mockMvc.perform(post("/artwork/addArtwork")
+     .contentType(MediaType.APPLICATION_JSON)
+     .content(objectMapper.writeValueAsString(artwork)))
+     .andExpect(status().isUnauthorized());
+     }
+
+     @Test
+     @WithMockUser(username = "staff@gmail.com", password="Staff@10", roles = {"STAFF"})
+     public void testAddArtwork_NotAuthorized() throws Exception {
+
+     Artwork artwork = new Artwork();
+     artwork.setTitle("Artwork Title");
+
+     when(artworkRepository.addArt(any(Artwork.class))).thenReturn(0);
+
+     mockMvc.perform(post("/artwork/addArtwork")
+     .contentType(MediaType.APPLICATION_JSON)
+     .content(objectMapper.writeValueAsString(artwork)))
+     .andExpect(status().isForbidden());
+     }
+
+
 }
 
